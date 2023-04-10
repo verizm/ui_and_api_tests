970c6f4542a0879323720adc7f77750fe5649a02
Lazunina Vera <lazunina@llcglobaladvisors.com>
2023-04-10 19:52:24 +0300

    2 commit

--- logger.py
+++ logger.py
@@ -6,7 +6,6 @@ LOGGER.setLevel(logging.INFO)
 handler = logging.StreamHandler(stream=sys.stdout)
 LOGGER.addHandler(handler)
 
-
 # commit from master
-#ДЛЯ 1 коммита в new branch
-
+# ДЛЯ 1 коммита в new branch
+# Для 2 коммита
